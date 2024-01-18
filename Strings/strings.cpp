9d5746aa6ffad5b13016c43b7ff1025b7af0139b
SHREYA <123317004+shrey-111@users.noreply.github.com>
2024-01-18 15:14:41 +0530

    Add files via upload

---
+++ Strings/strings.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<string>
+bool isPalindrome(std::string str) {
+    int left =0, right =str.length() -1;
+    while(left<right) {
+        if (str[left]!= str[right]) {
+            return false;
+        }
+        left++;
+        right--;
+    }
+    return true;
+}
+int main() {
+    std::string str;
+    std::cout << "Enter a string:";
+    std::cin >> str;
+    if (isPalindrome(str)) {
+        std::cout << str << "is a palindrome ." << std::endl;
+    }
+    else{
+        std::cout << str << "is not a pallindrome ." << std::endl;
+    }
+    return 0;
+
+    
+}
